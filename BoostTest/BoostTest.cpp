fa5f31d732bbf4ecf7d6e511d2f5cf4d04dd2844
qianyanglm <63186194+qianyanglm@users.noreply.github.com>
2023-08-13 10:37:19 +0800

    8.13

--- BoostTest/BoostTest.cpp
+++ BoostTest/BoostTest.cpp
@@ -10,71 +10,91 @@
 #include <vector>
 using namespace std;
 
+//线程池类
 class ThreadPool
 {
 public:
-    ThreadPool(size_t threads);
-    ~ThreadPool();
+    ThreadPool(size_t threads);//构造函数，传入线程数
+    ~ThreadPool();             //析构
+    //可变参数模板
+    //入队任务（传入参数和函数的参数）
     template<typename F, typename... Args>
     auto enqueue(F &&f, Args &&...args) -> std::future<typename std::result_of<F(Args...)>::type>;
 
 private:
-    std::vector<std::thread> _workers;// 所有工作线程
-    std::queue<std::function<void()>> _tasks;
+    std::vector<std::thread> _workers;       // 工作线程组
+    std::queue<std::function<void()>> _tasks;//任务队列
 
-    std::mutex _queueMutex;
-    std::condition_variable _condition;
-    bool _stop;// 线程池停止标志
+    //异步
+    std::mutex _queueMutex;            //队列互斥锁
+    std::condition_variable _condition;//条件变量
+    bool _stop;                        // 线程池停止标志
 };
 
-// 设置线程任务
+// 设置线程任务，启动一些工作线程
 ThreadPool::ThreadPool(size_t threads)
 {
+    //添加线程到工作线程组
     for (size_t i = 0; i < threads; ++i)
     {
-        // 每个线程需要做的事情很简单
+        // 线程内不断的从任务队列取任务执行
         // 1. 从任务队列中获取任务（需要保护临界区）
         // 2. 执行任务
         _workers.emplace_back([this] {
-            while (true)
+            for (;;)
             {
                 std::function<void()> task;
+
                 {
+                    // 拿锁(独占所有权式)
                     std::unique_lock<std::mutex> lock(this->_queueMutex);
-                    // 等待唤醒，条件是停止或者任务队列中有任务
+
+                    // 等待唤醒（等待条件成立），条件是停止或者任务队列中有任务
+
+                    /*执行条件变量等待的时候，已经拿到了锁(即lock已经拿到锁，没有阻塞)
+                    这里将会unlock释放锁，其他线程可以继续拿锁，但此处任然阻塞，等待条件成立
+                    一旦收到其他线程notify_*唤醒，则再次lock，然后进行条件判断
+                    当[return this->stop || !this->tasks.empty()]的结果为false将阻塞
+                    条件为true时候解除阻塞。此时lock依然为锁住状态*/
                     this->_condition.wait(lock, [this] { return this->_stop || !this->_tasks.empty(); });
-                    if (this->_stop && this->_tasks.empty()) return;
+
+                    // 如果线程池停止或者任务队列为空，结束返回
+                    if (this->_stop && this->_tasks.empty())
+                        return;
                     task = std::move(this->_tasks.front());
                     this->_tasks.pop();
                 }
+
                 task();
             }
         });
     }
 }
 
+//添加一个新的工作任务到线程池
 template<typename F, typename... Args>
 auto ThreadPool::enqueue(F &&f, Args &&...args) -> std::future<typename std::result_of<F(Args...)>::type>
 {
     using return_type = typename std::result_of<F(Args...)>::type;
 
-    // 将需要执行的任务函数打包（bind），转换为参数列表为空的函数对象
+    // 将任务函数和其参数绑定，构建一个packaged_task
     auto task = std::make_shared<std::packaged_task<return_type()>>(
             std::bind(std::forward<F>(f), std::forward<Args>(args)...));
 
+    //获取任务的future
     std::future<return_type> res = task->get_future();
-    {
-        std::unique_lock<std::mutex> lock(_queueMutex);
 
+    {
+        // 独占拿锁
+        std::unique_lock<std::mutex> lock(_queueMutex);
+        //不允许入队到已经停止的线程池
         if (_stop) throw std::runtime_error("enqueue on stopped ThreadPool");
 
-        // 利用lambda函数包装线程函数，使其符合function<void()>的形式
-        // 并且返回值可以通过future获取
-        _tasks.emplace([task]() {
-            (*task)();
-        });
+        // 将任务添加到任务队列
+        _tasks.emplace([task]() { (*task)(); });
     }
 
+    //发送通知，唤醒某一个工作线程执行任务
     _condition.notify_all();
     return res;
 }
@@ -82,10 +102,14 @@ auto ThreadPool::enqueue(F &&f, Args &&...args) -> std::future<typename std::res
 ThreadPool::~ThreadPool()
 {
     {
+        //拿锁
         std::unique_lock<std::mutex> lock(_queueMutex);
-        _stop = true;
+        _stop = true;//停止标志置true
     }
-    _condition.notify_all();// 唤醒所有线程，清空任务
+
+    // 通知所有工作线程，唤醒后因为stop为true了，所以都会结束
+    _condition.notify_all();
+    // 等待所有工作线程结束
     for (std::thread &worker: _workers)
     {
         worker.join();
@@ -94,30 +118,28 @@ ThreadPool::~ThreadPool()
 
 #include <chrono>
 #include <iostream>
-
-void PrintNumber(int num)
-{
-    std::cout << "Thread " << std::this_thread::get_id() << ": " << num << std::endl;
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
-}
+#include <vector>
 
 int main()
 {
-    ThreadPool threadPool(4);// 创建一个拥有4个线程的线程池
+    ThreadPool pool(4);//创建线程池，指定线程数量4
+    std::vector<std::future<int>> results;
 
-    for (int i = 0; i < 10; ++i)
+    for (int i = 0; i < 8; ++i)
     {
-        threadPool.enqueue(PrintNumber, i);// 将PrintNumber任务添加到线程池中
+        results.emplace_back(pool.enqueue(
+                [i] {
+                    std::cout << "hello " << i << std::endl;
+                    std::this_thread::sleep_for(std::chrono::seconds(1));
+                    std::cout << "world " << i << std::endl;
+                    return i * i;
+                }));
     }
 
-    // 添加更多任务...
-    auto future1 = threadPool.enqueue([]() { return 42; });
-    auto future2 = threadPool.enqueue([]() { return 100; });
-
-    std::cout << "future1的结果: " << future1.get() << std::endl;
-    std::cout << "future2的结果: " << future2.get() << std::endl;
-
-    // ThreadPool的析构函数将确保所有任务完成并且线程被加入
+    // 遍历存储在 results 中的每个任务的返回结果
+    for (auto &&result: results)
+        std::cout << result.get() << ' ';
+    std::cout << std::endl;
 
     return 0;
 }
