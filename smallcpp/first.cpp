a2074b1b699844e82473257cab845227f29a2f6e
qianyanglm <63186194+qianyanglm@users.noreply.github.com>
2023-08-24 22:10:55 +0800

    8.24

--- smallcpp/first.cpp
+++ smallcpp/first.cpp
@@ -1,49 +1,101 @@
-﻿#include <future>
+﻿#include <Windows.h>
+#include <future>
 #include <iostream>
 #include <thread>
 using namespace std;
+#define _WINDOWSLJQ_
 
-int mythread(int mypar)
+class A
 {
-    cout << "mythread() start threadid = " << std::this_thread::get_id() << endl;
-    std::chrono::milliseconds dura(1000);
-    std::this_thread::sleep_for(dura);
-    //    cout << "mythread() end threadid = " << std::this_thread::get_id() << endl;
-    return 5;
-}
+public:
+    A()
+    {
+#ifdef _WINDOWSLJQ_
+        InitializeCriticalSection(&my_winsec);//初始化临界区
+#endif
+    }
 
-void mythread2(std::future<int> &tmpf)
-{
-    cout << "mythread2()start"
-         << " threadid = " << std::this_thread::get_id() << endl;
-    auto result = tmpf.get();
-    //    auto result1 = tmpf.get();
-    cout << "mythread2 result =" << result << endl;
-    return;
-}
+    virtual ~A()
+    {
+#ifdef _WINDOWSLJQ_
+        DeleteCriticalSection(&my_winsec);//释放临界区
+#endif
+    }
 
-int myThread(int num)
-{
-    cout << "Thread start id = " << this_thread::get_id() << endl;
-    chrono::milliseconds time(1000);
-    this_thread::sleep_for(time);
-    cout << "Thread end id = " << this_thread::get_id() << endl;
-    return num * 2;
-}
+    //把收到的消息放入队列的线程
+    void inMsgRecvQueue()
+    {
+        for (int i = 0; i < 100000; ++i)
+        {
+            cout << "inMsgRecvQueue()执行，插入一个元素: " << i << endl;
+#ifdef _WINDOWSLJQ_
+            EnterCriticalSection(&my_winsec);//进入临界区
+            msgRecvQueue.push_back(i);
+            LeaveCriticalSection(&my_winsec);//离开临界区
+#else
+            my_mutex.lock();
+            msgRecvQueue.push_back(i);
+            my_mutex.unlock();
+#endif
+        }
+    }
+
+    bool outMsgLULProc(int &command)
+    {
+#ifdef _WINDOWSLJQ_
+        EnterCriticalSection(&my_winsec);
+        if (!msgRecvQueue.empty())
+        {
+            int command = msgRecvQueue.front();
+            msgRecvQueue.pop_front();
+            LeaveCriticalSection(&my_winsec);
+            return true;
+        }
+        LeaveCriticalSection(&my_winsec);
+#else
+        my_mutex.lock();
+        if (!msgRecvQueue.empty())
+        {
+            int command = msgRecvQueue.front();
+            msgRecvQueue.pop_front();
+            LeaveCriticalSection(&my_winsec);
+            return true;
+        }
+        my_mutex.unlock();
+#endif
+        return false;
+    }
+
+    void outMsgRecvQueue()
+    {
+        int command = 0;
+        for (int i = 0; i < 100000; ++i)
+        {
+            bool result = outMsgLULProc(command);
+            if (result == true)
+            {
+                cout << "outMsgRecvQueue()执行了，从容器中取出一个元素" << command << endl;
+            }
+            else
+            {
+                cout << "outMsgRecvQueue()执行了，但目前收消息队列中是空元素" << i << endl;
+            }
+        }
+        cout << "end" << endl;
+    }
+
+private:
+    std::list<int> msgRecvQueue;
+    std::mutex my_mutex;
+#ifdef _WINDOWSLJQ_
+    //windows下叫临界区，类似于互斥量mutex
+    CRITICAL_SECTION my_winsec;
+#endif
+};
 
 int main()
 {
     cout << "main threadid = " << std::this_thread::get_id() << endl;
-    std::packaged_task<int(int)> mypt(myThread);
-    std::thread t1(std::ref(mypt), 1);
-    t1.join();
-    std::shared_future<int> result = mypt.get_future();
-    //    std::thread t2(mythread2, std::ref(result));
-    //    t2.join();
-    //    std::thread t3(mythread2, std::ref(result));
-    //    t3.join();
-    cout << "result = " << result.get() << endl;
-    cout << "result = " << result.get() << endl;
 
     cout << "Main function execution finished! " << endl;
     return 0;
