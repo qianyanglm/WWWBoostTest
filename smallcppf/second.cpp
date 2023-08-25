b2f493bad4ba0016f7aa065dc2a2553c2e9f5f72
qianyanglm <63186194+qianyanglm@users.noreply.github.com>
2023-08-25 16:08:27 +0800

    8.25

--- smallcppf/second.cpp
+++ smallcppf/second.cpp
@@ -4,9 +4,17 @@ using namespace std;
 class A
 {
 public:
+    A() {}
+
+    ~A() {}
+
+    int m_i;
 };
 
 int main()
 {
+    char *ppoint = new char[55];
+    memset(ppoint, 0, 55);
+    delete[] ppoint;
     return 0;
 }
