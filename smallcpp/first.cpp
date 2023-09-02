1152002021e6f21e06d6b6140aa284d46e64deb0
qianyanglm <63186194+qianyanglm@users.noreply.github.com>
2023-09-02 20:29:21 +0800

    9.2

--- smallcpp/first.cpp
+++ smallcpp/first.cpp
@@ -1,28 +1,25 @@
-﻿#include <iostream>
-#include <memory>
-#include <vector>
+﻿// MyProject.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+//公众号：程序员速成   ，内含优质视频教程，欢迎关注
 
-using namespace std;
+#include <atomic>
+#include <ctime>
+#include <iostream>
+#include <list>
+#include <mutex>
+#include <thread>
+#include <vector>
+#define _CRTDBG_MAP_ALLOC
+
+using namespace std;//后面再使用诸如std::cout时就可以简写成cout了；
 
 class A
 {
 public:
     static void *operator new(size_t size);
     static void operator delete(void *phead);
-
-    static int m_iCount;      //分配计数统计，new一次+1
-    static int m_iMallocCount;//统计malloc次数，malloc一次+1
-
-    A()
-    {
-        cout << "A的构造函数执行了" << endl;
-    }
-
-    ~A()
-    {
-        cout << "A的析构函数执行了" << endl;
-    }
-
+    static int m_iCount;      //用于分配计数统计，每new一次+1
+    static int m_iMallocCount;//用于统计malloc次数，每malloc一次+1
 private:
     A *next;
     static A *m_FreePosi;    //总是指向一块可以分配出去的内存的首地址
@@ -31,29 +28,25 @@ private:
 
 void *A::operator new(size_t size)
 {
-    // std::cout << "A::operator new 被调用了!" << std::endl;
-    // A *ppoint = (A *) malloc(size);
-    // return ppoint;
-
-    //不使用传统方式实现。使用内存池实现
-
+    //A *ppoint = (A *)malloc(size); //不再用传统方式实现，而是用内存池实现
+    //return ppoint;
     A *tmplink;
     if (m_FreePosi == nullptr)
     {
         //为空，我们要申请内存，申请的是很大一块内存
-        size_t realsize = m_sTrunkCount * size;
-        m_FreePosi = reinterpret_cast<A *>(new char[realsize]);
-
+        size_t realsize = m_sTrunkCount * size;                //申请m_TrunkCount这么多倍的内存
+        m_FreePosi = reinterpret_cast<A *>(new char[realsize]);//这是传统new，调用底层传统malloc
         tmplink = m_FreePosi;
 
-        //把分配出来的内存链接起来，后续使用(单链表)
-        for (; tmplink != &m_FreePosi[m_iMallocCount - 1]; ++tmplink)
+        //把分配出来的这一大块内存链起来，供后续使用
+        for (; tmplink != &m_FreePosi[m_sTrunkCount - 1]; ++tmplink)
         {
             tmplink->next = tmplink + 1;
         }
         tmplink->next = nullptr;
         ++m_iMallocCount;
     }
+
     tmplink = m_FreePosi;
     m_FreePosi = m_FreePosi->next;
     ++m_iCount;
@@ -62,24 +55,29 @@ void *A::operator new(size_t size)
 
 void A::operator delete(void *phead)
 {
-    // cout << "A::operator delete 被调用了" << endl;
-    // free(phead);
+    free(phead);
 
-    //不使用传统方式实现。使用内存池实现
-    (static_cast<A *>(phead))->next = m_FreePosi;
-    m_FreePosi = static_cast<A *>(phead);
+    // //free(phead);  //不再用传统方式实现，针对内存池有特别的实现
+    // (static_cast<A *>(phead))->next = m_FreePosi;
+    // m_FreePosi = static_cast<A *>(phead);
 }
 
+//----------------------------------------
 int A::m_iCount = 0;
 int A::m_iMallocCount = 0;
 
 A *A::m_FreePosi = nullptr;
 int A::m_sTrunkCount = 5;//一次分配5倍的该类内存作为内存池的大小
 
+//int A::m_sTrunkCount = 500;
+
 int main()
 {
-    A *pa = new A();
-    delete pa;
+    {
+        cout << "hello" << endl;
+    }
+
+    _CrtDumpMemoryLeaks();
 
     return 0;
 }
--- smallcppf/second.cpp
+++ smallcppf/second.cpp
@@ -1,37 +1,98 @@
-﻿// debug_malloc.cpp
+﻿#include <iostream>
+#include <memory>
+#include <vector>
 // compile by using: cl /EHsc /W4 /D_DEBUG /MDd debug_malloc.cpp
 #define _CRTDBG_MAP_ALLOC
 #include <algorithm>
 #include <crtdbg.h>
 #include <iostream>
-#include <memory>
 #include <stdlib.h>
-#include <vector>
-
 using namespace std;
 
-// void mydeleter(string *pdel)
-// {
-//     delete pdel;
-//     pdel = nullptr;
-// }
-
-void mydeleter2(string *pdel)
+class A
 {
-    delete pdel;
-    pdel = nullptr;
+public:
+    static void *operator new(size_t size);
+    static void operator delete(void *phead);
+
+    static int m_iCount;      //分配计数统计，new一次+1
+    static int m_iMallocCount;//统计malloc次数，malloc一次+1
+
+    A()
+    {
+        cout << "A的构造函数执行了" << endl;
+    }
+
+    ~A()
+    {
+        cout << "A的析构函数执行了" << endl;
+    }
+
+private:
+    A *next;
+    static A *m_FreePosi;    //总是指向一块可以分配出去的内存的首地址
+    static int m_sTrunkCount;//一次分配多少倍该类的内存
+};
+
+void *A::operator new(size_t size)
+{
+    // std::cout << "A::operator new 被调用了!" << std::endl;
+    // A *ppoint = (A *) malloc(size);
+    // return ppoint;
+
+    //不使用传统方式实现。使用内存池实现
+
+    A *tmplink;
+    if (m_FreePosi == nullptr)
+    {
+        //为空，我们要申请内存，申请的是很大一块内存
+        size_t realsize = m_sTrunkCount * size;
+        m_FreePosi = reinterpret_cast<A *>(new char[realsize]);
+        tmplink = m_FreePosi;
+
+        //把分配出来的内存链接起来，后续使用(单链表)
+        for (; tmplink != &m_FreePosi[m_sTrunkCount - 1]; ++tmplink)
+        {
+            tmplink->next = tmplink + 1;
+        }
+        tmplink->next = nullptr;
+        ++m_iMallocCount;
+    }
+
+    tmplink = m_FreePosi;
+    m_FreePosi = m_FreePosi->next;
+    ++m_iCount;
+    return tmplink;
 }
+
+void A::operator delete(void *phead)
+{
+    A *p = static_cast<A *>(phead);
+    // p->~A();// 先调用析构函数
+    // 将 p 链接到 m_FreePosi
+    p->next = m_FreePosi;
+    m_FreePosi = p;
+    --m_iCount;
+    free(p);
+}
+
+int A::m_iCount = 0;
+int A::m_iMallocCount = 0;
+
+A *A::m_FreePosi = nullptr;
+int A::m_sTrunkCount = 5;//一次分配5倍的该类内存作为内存池的大小
 
 int main()
 {
     {
-        int *arr = new int[10];
-        arr[0] = 1;
-        cout << arr[0] << endl;
-        delete arr;
+        for (int i = 0; i < 15; ++i)
+        {
+            A *pa = new A();
+            printf("%p\n", pa);
+            delete pa;
+        }
     }
-
-    // _CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_DEBUG);
     _CrtDumpMemoryLeaks();
+
     return 0;
 }
