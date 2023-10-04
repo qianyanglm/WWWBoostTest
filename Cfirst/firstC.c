cb907eeb587c07c40a37dc8589a5dd5bb5a77392
qianyanglm <63186194+qianyanglm@users.noreply.github.com>
2023-10-04 19:14:59 +0800

    10.4

--- Cfirst/firstC.c
+++ Cfirst/firstC.c
@@ -1,1 +1,20 @@
-﻿
+﻿#include <stdio.h>
+
+void test(int arg[])
+{
+    for (unsigned int i = 0; i < sizeof(arg); ++i)
+    {
+        printf("%d\n", arg[i]);
+    }
+}
+
+int main()
+{
+    int a[10] = {
+            1,
+            2,
+            3,
+    };
+    test(a);
+    return 0;
+}
--- smallcpp/first.cpp
+++ smallcpp/first.cpp
@@ -1,41 +1,21 @@
-﻿#include <stdio.h>
-#include <stdlib.h>
+﻿#include <iostream>
+using namespace std;
 
-void *operator new(size_t sz)
+void test(int arg[])
 {
-    printf("operator new:%d Bytes\n", sz);
-    void *m = malloc(sz);
-    if (!m)
-        puts("out of memory");
-    return m;
+    for (unsigned int i = 0; i < sizeof(arg); ++i)
+    {
+        cout << arg[i] << endl;
+    }
 }
-
-void operator delete(void *m)
-{
-    puts("operator delete");
-    free(m);
-}
-
-class S
-{
-    int i[100];
-
-public:
-    S() { puts("S:S()"); }
-
-    ~S() { puts("~S::S()"); }
-};
 
 int main()
 {
-    puts("(1)Creating & Destroying an int");
-    int *p = new int(47);
-    delete p;
-    puts("(2)Creating & Destroying an S");
-    S *s = new S;
-    delete s;
-    puts("(3)Creating & Destroying S[3]");
-    S *sa = new S[3];
-    delete[] sa;
+    int a[10] = {
+            1,
+            2,
+            3,
+    };
+    test(a);
     return 0;
 }
