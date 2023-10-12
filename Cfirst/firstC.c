57f44ca64bd6b8905825d73d9f6b3307742d044d
qianyanglm <63186194+qianyanglm@users.noreply.github.com>
2023-10-12 08:44:11 +0800

    10.12

--- Cfirst/firstC.c
+++ Cfirst/firstC.c
@@ -3,19 +3,5 @@
 
 int main()
 {
-    const char str1[] = "abcde2fghi3jk4l";
-    const char str2[] = "34";
-    char *ret;
-
-    ret = strpbrk(str1, str2);
-    if (ret)
-    {
-        printf("第一个匹配的字符是： %c\n", *ret);
-    }
-    else
-    {
-        printf("未找到字符");
-    }
-
     return (0);
 }
