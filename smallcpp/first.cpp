bd9cdb923015257de26bc95f59a7c16a02ba4d55
qianyanglm <63186194+qianyanglm@users.noreply.github.com>
2023-11-14 15:30:13 +0800

    11.14

--- smallcpp/first.cpp
+++ smallcpp/first.cpp
@@ -1,34 +1,26 @@
-﻿#include "fourthfour.h"
-
-#include <iostream>
+﻿#include <iostream>
 #include <ostream>
+
+struct student
+{
+    char mark;
+    long num;
+    float score;
+};
+
+union test
+{
+    char mark;
+    long num;
+    float score;
+};
+
+enum test1 { code = 0,
+             code1,
+             code2 };
 
 int main()
 {
-    std::shared_ptr<Factory> factory;
-    std::shared_ptr<Movie> product;
-
-    // 这里假设从配置中读到的是Chinese(运行时决定的)
-    std::string conf = "China";
-
-    // 程序根据当前配置或环境选择创建者的类型
-    if (conf == "China")
-    {
-        factory = std::make_shared<ChineseProducer>();
-    }
-    else if (conf == "Japan")
-    {
-        factory = std::make_shared<JapaneseProducer>();
-    }
-    else if (conf == "America")
-    {
-        factory = std::make_shared<AmericanProducer>();
-    }
-    else
-    {
-        std::cout << "error conf" << std::endl;
-    }
-
-    product = factory->get_movie();
-    std::cout << "获取一部电影: " << product->get_a_movie() << std::endl;
+    std::cout << code1 << std::endl;
+    return 0;
 }
