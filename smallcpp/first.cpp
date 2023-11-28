3a909ee765d59e2b38fc1485e83f849cdf0bff79
qianyanglm <63186194+qianyanglm@users.noreply.github.com>
2023-11-28 14:45:32 +0800

    11.28

--- smallcpp/first.cpp
+++ smallcpp/first.cpp
@@ -1,36 +1,98 @@
-﻿#include <iostream>
+﻿#include <assert.h>
+#include <iostream>
+#include <string>
 using namespace std;
 
-void bubbleSort(int arr[], int n) {
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                // 交换相邻元素
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
+template<class type>
+class ablist
+{
+public:
+    int GetLength()
+    {
+        return length;
     }
+
+    virtual type GetElements(int index);
+    virtual bool Expand(int) = 0;
+    virtual void Empty() = 0;
+    virtual bool InsertTarget(type value, int index) = 0;
+    virtual type RemoveElement(int index) = 0;
+    virtual type RemoveTarget(type target) = 0;
+
+protected:
+    int length = 0;
+};
+
+template<class type>
+class SeqList: public ablist<type>
+{
+protected:
+    type *elements;
+    int maxsize;
+
+public:
+    SeqList(int maxs = 10);
+    SeqList<type> &Copy(SeqList<type> &s);
+    bool Expand(int);
+    bool SetElements(type x, int index);
+    type GetElements(int index);
+
+    void Empty()
+    {
+        if (elements != NULL) delete[] elements;
+    }
+
+    type RemoveElement(int index);
+    type RemoveTarget(type target);
+    bool InsertTarget(type value, int index);
+
+    SeqList<type> &operator=(SeqList<type> &s)
+    {
+        Copy(s);
+        return *this;
+    }
+
+    // friend ostream& operator<<(ostream&,  SeqList<type>&);
+    ~SeqList()
+    {
+        if (elements != NULL) delete[] elements;
+    }
+};
+
+template<class type>
+SeqList<type>::SeqList(int maxs)
+{
+    maxsize = maxs > 10 ? maxs : 10;
+    this->length = 0;
+    elements = NULL;
+    elements = new type[maxs];
+    assert(elements != NULL);
 }
 
-int main() {
-    int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
-    int n = sizeof(arr) / sizeof(arr[0]);
+// 将原顺序表赋值拷贝给新顺序表
+template<class type>
+SeqList<type> &SeqList<type>::Copy(SeqList<type> &s)
+{
+    if (elements)
+        delete[] elements;
+    maxsize = 0;
+    length = 0;
+    elements = new type[s.maxsize];
 
-    cout << "排序前的数组：";
-    for (int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
+    if (elements)
+    {
+        maxsize = s.maxsize;
+        length = s.length;
+        // memmove 将s.elements的内容复制到elements,长度为size
+        memmove(elements, s.elements, sizeof(type) * maxsize);
     }
-    cout << endl;
-
-    bubbleSort(arr, n);
-
-    cout << "排序后的数组：";
-    for (int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
-
-    return 0;
+    return *this;
 }
+
+template<class type>
+bool SeqList<type>::Expand(int newmaxsize)
+{
+    type *p, *q;// 新建两个指针,一个用来指向新list,一个保存原list
+    if (newmaxsize <= maxsize)
+    {
+        cout << "New m
--- smallcppf/second.cpp
+++ smallcppf/second.cpp
@@ -1,99 +1,14 @@
-﻿#include <iostream>
-#include <memory>
-#include <vector>
-// compile by using: cl /EHsc /W4 /D_DEBUG /MDd debug_malloc.cpp
-#define _CRTDBG_MAP_ALLOC
-#include <algorithm>
-#include <crtdbg.h>
-#include <iostream>
-#include <stdlib.h>
-using namespace std;
-
-class A
+﻿class Parent
 {
-public:
-    static void *operator new(size_t size);
-    static void operator delete(void *phead);
-
-    static int m_iCount;      //分配计数统计，new一次+1
-    static int m_iMallocCount;//统计malloc次数，malloc一次+1
-
-    A()
-    {
-        cout << "A的构造函数执行了" << endl;
-    }
-
-    ~A()
-    {
-        cout << "A的析构函数执行了" << endl;
-    }
-
-private:
-    A *next;
-    static A *m_FreePosi;    //总是指向一块可以分配出去的内存的首地址
-    static int m_sTrunkCount;//一次分配多少倍该类的内存
+protected:
+    int protected_member;
 };
 
-void *A::operator new(size_t size)
+class Child: public Parent
 {
-    // std::cout << "A::operator new 被调用了!" << std::endl;
-    // A *ppoint = (A *) malloc(size);
-    // return ppoint;
-
-    //不使用传统方式实现。使用内存池实现
-
-    A *tmplink;
-    if (m_FreePosi == nullptr)
+public:
+    void access()
     {
-        //为空，我们要申请内存，申请的是很大一块内存
-        size_t realsize = m_sTrunkCount * size;
-        m_FreePosi = reinterpret_cast<A *>(new char[realsize]);
-        tmplink = m_FreePosi;
-
-        //把分配出来的内存链接起来，后续使用(单链表)
-        for (; tmplink != &m_FreePosi[m_sTrunkCount - 1]; ++tmplink)
-        {
-            tmplink->next = tmplink + 1;
-        }
-        tmplink->next = nullptr;
-        ++m_iMallocCount;
+        protected_member = 10;// 可以访问 protected_member
     }
-
-    tmplink = m_FreePosi;
-    m_FreePosi = m_FreePosi->next;
-    ++m_iCount;
-    return tmplink;
-}
-
-void A::operator delete(void *phead)
-{
-    A *p = static_cast<A *>(phead);
-    // p->~A();// 先调用析构函数
-    // 将 p 链接到 m_FreePosi
-    p->next = m_FreePosi;
-    m_FreePosi = p;
-    --m_iCount;
-    free(p);
-}
-
-int A::m_iCount = 0;
-int A::m_iMallocCount = 0;
-
-A *A::m_FreePosi = nullptr;
-int A::m_sTrunkCount = 5;//一次分配5倍的该类内存作为内存池的大小
-
-int main()
-{
-    {
-        for (int i = 0; i < 15; ++i)
-        {
-            A *pa = new A();
-            printf("%p\n", pa);
-            delete pa;
-        }
-    }
-    _CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_DEBUG);
-    _CrtDumpMemoryLeaks();
-
-    return 0;
-}
+};
